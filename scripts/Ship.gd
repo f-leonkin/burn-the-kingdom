5489ebb719ff0073ca82f4baec29cc8b7697a3f9
Fyodor Leonkin <leonkin@tuta.io>
2020-06-20 00:13:36 +0300

    Make sail material unshaded on Android

--- scripts/Ship.gd
+++ scripts/Ship.gd
@@ -4,7 +4,13 @@ onready var spawn = $Spatial/EnemySpawn
 
 export(String, "Catapult", "Scorpio", "None") var weapon_type = "Catapult"
 
+
 func _ready():
+	if OS.get_name() == "Android":
+		var sail_material = $Spatial/Mesh.mesh.surface_get_material(2)
+		sail_material.flags_unshaded = true
+		$Spatial/Mesh.mesh.surface_set_material(2, sail_material)
+	
 	if weapon_type == "None":
 		return
 	var weapon
